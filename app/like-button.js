b49015b4aaa665832c463ec574ecabeaddb464c3
Mini <a100567@alunos.uminho.pt>
2024-10-22 00:06:52 +0100

    lazy loading

---
+++ app/like-button.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+
+export default function LikeButton() {
+    const [likes, setLikes] = useState(0);
+    function handleClick() {
+        setLikes(likes + 1);
+      }
+     
+      return <button className='like-button' onClick={handleClick}>Like ({likes})</button>;
+}
